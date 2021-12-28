5015e19a68ee8790b4c67a43ad95c889476ddca5
David Garcia <david.a.g.r93@gmail.com>
2021-12-28 19:34:30 +0100

    Improve tab bar

--- rindus/Features/TabBar/Routers/MainTabBarRouter.swift
+++ rindus/Features/TabBar/Routers/MainTabBarRouter.swift
@@ -6,11 +6,22 @@
 //
 
 import Foundation
+import UIKit
 
 class MainTabBarRouter {
     static func makeView() -> MainTabBarViewController {
         let views = [MainWeatherRouter.makeView(),
                      MainWebRouter.makeView(url: "https://www.c-and-a.com")]
+        for i in 0..<views.count {
+            var tabBarItem: UITabBarItem
+            switch i {
+            case 0:
+                tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: i)
+            default:
+                tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: i)
+            }
+            views[i].tabBarItem = tabBarItem
+        }
         let controller = MainTabBarViewController(views)
         return controller
     }
